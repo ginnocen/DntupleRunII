9aaa3fdf25f1741b1787a62110d2189af5fd762a
Yen-Jie Lee <yenjie@mit.edu>
2016-03-03 11:49:43 +0100

    Trigger efficiency correction for D mesons

---
+++ TriggerStudies/triggerEfficiency.C
@@ -0,0 +1,18 @@
+#include <TF1.h>
+
+// Yen-Jie: correction factors for L1 + HLT trigger efficiency for D meson
+
+float triggerEfficiency(double pt, bool isPbPb)
+{
+   float val=1;
+   
+   if (isPbPb) {
+      // PbPb collisions
+      val = 1;
+   } else {
+      // pp collisions
+      val = 1;
+   }
+   
+   return val;
+}
