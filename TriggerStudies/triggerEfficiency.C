a6fea0f4c188c24dd9b0a269798d905223f8d856
Yen-Jie Lee <yenjie@mit.edu>
2016-03-04 16:37:02 +0100

    Added PbPb correction

--- TriggerStudies/triggerEfficiency.C
+++ TriggerStudies/triggerEfficiency.C
@@ -7,10 +7,33 @@ float triggerEfficiency(double pt, double HLT, bool isPbPb)
    float val=1;
    
    if (isPbPb) {
-      // PbPb collisions
+      // correction factor for PbPb collisions
       val = 1;
+      TF1 *fL1 = new TF1("fL1","TMath::Erf(x*[1]+[2])*0.5*(1-[0])+0.5*(1+[0])");
+      if (HLT==20) {
+         fL1->SetParameters(-4.5400e3,3.09974e-1,2.31049);
+      } else if (HLT==40) {
+         fL1->SetParameters(-4.05647e3,8.77473e-3,3.1443);
+      } else if (HLT==60) {
+         fL1->SetParameters(7.0734e-2,6.76907e-2,-2.31850);               
+      } else {
+         cout <<"Oh no, where did you find this HLT path??? I don't remember this value "<<HLT<<". This is a disaster! We need to go to Brocard to buy more cakes..."<<endl;
+	 return 1e10; 
+      }
+
+      // HLT, at high pt things are consistent with 0.9275+-0.0055 for the moment
+      if (pt<HLT) {
+         val = 1e10; // veto
+	 cout <<"using D candidate below HLT threshold!!"<<endl;
+      } else {
+         // 
+         val = 0.9275;
+      }
+      
+      val *= fL1->Eval(pt);
+      
    } else {
-      // pp collisions
+      // correction factor for pp collisions
       TF1 *fL1 = new TF1("fL1","TMath::Erf(x*[1]+[2])*0.5*(1-[0])+0.5*(1+[0])");
       if (HLT==8) {
          fL1->SetParameters(1.81837e-1,1.54009e-1,-1.66545);
