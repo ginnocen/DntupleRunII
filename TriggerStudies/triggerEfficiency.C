f3a3ca2c4d463cee630246f1234ba1b87547b551
ginnocen <ginnocen@cern.ch>
2016-03-06 10:27:16 +0100

    solve conflict

--- TriggerStudies/triggerEfficiency.C
+++ TriggerStudies/triggerEfficiency.C
@@ -1,4 +1,4 @@
-#include <TF1.h>
+	#include <TF1.h>
 
 // Yen-Jie: correction factors for L1 + HLT trigger efficiency for D meson
 
@@ -14,17 +14,19 @@ float triggerEfficiency(double pt, double HLT, bool isPbPb)
          fL1->SetParameters(-4.5400e3,3.09974e-1,2.31049);
       } else if (HLT==40) {
          fL1->SetParameters(-4.05647e3,8.77473e-3,3.1443);
+      } else if (HLT==50) {
+         fL1->SetParameters(2.01935e-1,7.43858e-2,-2.0107);               
       } else if (HLT==60) {
          fL1->SetParameters(7.0734e-2,6.76907e-2,-2.31850);               
-      } else {
-         std::cout <<"Oh no, where did you find this HLT path??? I don't remember this value "<<HLT<<". This is a disaster! We need to go to Brocard to buy more cakes..."<<std::endl;
+     } else {
+         cout <<"Oh no, where did you find this HLT path??? I don't remember this value "<<HLT<<". This is a disaster! We need to go to Brocard to buy more cakes..."<<endl;
 	 return 1e10; 
       }
 
       // HLT, at high pt things are consistent with 0.9275+-0.0055 for the moment
       if (pt<HLT) {
          val = 1e10; // veto
-	 std::cout <<"using D candidate below HLT threshold!!"<<std::endl;
+	 cout <<"using D candidate below HLT threshold!!"<<endl;
       } else {
          // 
          val = 0.9275;
@@ -44,14 +46,14 @@ float triggerEfficiency(double pt, double HLT, bool isPbPb)
       }	else if (HLT==30) {
          fL1->SetParameters(6.19120e-3,1.19415e-1,-2.24150);
       } else {
-         std::cout <<"Oh no, where did you find this HLT path??? I don't remember this value "<<HLT<<". This is a disaster! We need to go to Brocard to buy more cakes..."<<std::endl;
+         cout <<"Oh no, where did you find this HLT path??? I don't remember this value "<<HLT<<". This is a disaster! We need to go to Brocard to buy more cakes..."<<endl;
 	 return 1e10; 
       }
       
       // HLT, at high pt things are consistent with 1, put 1 for the moment
       if (pt<HLT) {
          val = 1e10; // veto
-	 std::cout <<"using D candidate below HLT threshold!!"<<std::endl;
+	 cout <<"using D candidate below HLT threshold!!"<<endl;
       } else {
          // 
          val = 1;
