f7b5edd970758ea9f713882b05dd2c7352017d6b
yjlee <yenjie.lee@cern.ch>
2016-03-09 04:31:14 -0500

    update and add yield extraction sys

--- Systematics/systematics.C
+++ Systematics/systematics.C
@@ -19,6 +19,10 @@ double ppDMesonSelection	= 11.4;			// TODO: old number from 2011, to be updated
 double ppBFeedDownCorrection	= 0;			// TODO
 double ppMCPtSpectra		= 0;			// TODO
 
+TF1 *fppSignalExtractionSig = new TF1("fppSignalExtractionSig","[0]+[1]/(x)+[2]/x/x+[3]*x");
+TF1 *fppSignalExtractionBkg = new TF1("fppSignalExtractionBkg","[0]+[1]/(x)+[2]/x/x+[3]*x");
+
+
 // =============================================================================================================
 // PbPb uncertainty
 // =============================================================================================================
@@ -33,14 +37,27 @@ double PbPbDMesonSelection	= 11.4;			// TODO: old number from 2011, to be update
 double PbPbBFeedDownCorrection	= 0;			// TODO
 double PbPbMCPtSpectra		= 0;			// TODO
 
+TF1 *fPbPbSignalExtractionSig = new TF1("fPbPbSignalExtractionSig","[0]+[1]/(x)+[2]/x/x+[3]*x");
+TF1 *fPbPbSignalExtractionBkg = new TF1("fPbPbSignalExtractionBkg","[0]+[1]/(x)+[2]/x/x+[3]*x");
+
+bool initialized = 0;
+
+void initialization()
+{
+   fppSignalExtractionSig->SetParameters(-2.783,58.6,-161.6,0.1142);
+   fppSignalExtractionBkg->SetParameters(-6.024,122,-314.1,0.1775);
+   fPbPbSignalExtractionSig->SetParameters(-6.024,122,-314.1,0.1775);  
+   fPbPbSignalExtractionBkg->SetParameters(6.741,-45.35,337,-0.04263);
+}
+
 // =============================================================================================================
 // RAA systematics
 // =============================================================================================================
 float systematicsForRAA(double pt, double HLT=0)
 {
+   if (!initialized) initialization();
    double sys=0;
    sys+=ppLumiUncertainty*ppLumiUncertainty;
-   //sys+=ppSignalExtraction*ppSignalExtraction;
    sys+=ppDMesonSelection*ppDMesonSelection;
    sys+=ppBFeedDownCorrection*ppBFeedDownCorrection;
    sys+=ppMCPtSpectra*ppMCPtSpectra;
@@ -49,7 +66,13 @@ float systematicsForRAA(double pt, double HLT=0)
    	else  sys+=PbPbLumiUncertainty*PbPbLumiUncertainty;
    
    sys+=TAAUncertainty0to100*TAAUncertainty0to100;
-   //sys+=PbPbSignalExtraction*PbPbSignalExtraction;
+
+
+   sys+=(fPbPbSignalExtractionSig->Eval(pt)-fppSignalExtractionSig->Eval(pt))*
+        (fPbPbSignalExtractionSig->Eval(pt)-fppSignalExtractionSig->Eval(pt));
+   sys+=(fPbPbSignalExtractionBkg->Eval(pt)-fppSignalExtractionBkg->Eval(pt))*
+   	(fPbPbSignalExtractionBkg->Eval(pt)-fppSignalExtractionBkg->Eval(pt));
+
    sys+=PbPbDMesonSelection*PbPbDMesonSelection;
    sys+=PbPbBFeedDownCorrection*PbPbBFeedDownCorrection;
    sys+=PbPbMCPtSpectra*PbPbMCPtSpectra;
@@ -62,6 +85,7 @@ float systematicsForRAA(double pt, double HLT=0)
 // =============================================================================================================
 float systematicsForRCP(double pt, double HLT=0)
 {
+   if (!initialized) initialization();
    return 0.2;
 
 }
@@ -72,6 +96,7 @@ float systematicsForRCP(double pt, double HLT=0)
 // =============================================================================================================
 float systematicsPP(double pt, double HLT=0)
 {
+   if (!initialized) initialization();
    double sys=0;
    sys+=(ppTrackingEfficiency*2)*(ppTrackingEfficiency*2);
    sys+=ppLumiUncertainty*ppLumiUncertainty;
@@ -88,6 +113,7 @@ float systematicsPP(double pt, double HLT=0)
 // =============================================================================================================
 float systematicsPbPb(double pt, double HLT=0)
 {
+   if (!initialized) initialization();
    double sys=0;
    sys+=(ppTrackingEfficiency*2)*(ppTrackingEfficiency*2);
    if (pt<20) sys+=PbPbNMBUncertainty*PbPbNMBUncertainty;
