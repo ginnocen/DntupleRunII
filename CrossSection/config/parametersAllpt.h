1dbcdad56e49b78aa4e086825db3ad9bd85141a8
ginnocen <ginnocen@cern.ch>
2016-03-15 11:02:25 +0100

    adding files and closure tests

---
+++ CrossSection/config/parametersAllpt.h
@@ -0,0 +1,11 @@
+const int nBins=19;
+double ptBins[nBins+1] = {2.,3.,4.,5.,6.,8.,10.,15.,20.,25.,30.,35.,40.,45.,50.,55.,60,70.,80,100};
+const int nBinsReweight=50;
+double ptBinsReweight[nBinsReweight+1] = {0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100};
+
+const int BIN_NUM= 1196;
+const int HMIN=1;
+const int HMAX=300;
+
+const double binsize=((double)(HMAX)-(double)(HMIN))/(double)(BIN_NUM);
+Double_t BRchain=0.0388;
