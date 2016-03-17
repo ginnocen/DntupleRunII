8f8ab16101e00b438616039bb3da78fb49d637f9
yjlee <yenjie.lee@cern.ch>
2016-03-17 10:48:51 -0400

    update

--- CrossSection/bFeedDown/bFeedDownCorrection.C
+++ CrossSection/bFeedDown/bFeedDownCorrection.C
@@ -4,6 +4,24 @@
 
 float bFeedDownFraction(double pt, double HLT, bool isPbPb)
 {
-   // return 0 for the moment
-   return 0;
+   double val;
+   if (!isPbPb) { // pp
+      if(pt>20&&pt<30) { 
+         // 77.0+- 1.7%
+         val = 1-0.77;
+      } else if (pt>30&&pt<40) {  
+         // 75.3 +- 1.6%
+	 val = 1-0.75;
+      } else if (pt>40&&pt<50) {
+         // 83.3% +- 3.5%
+	 val = 1-0.833;
+      } else if (pt>50) {
+         // 82.4% +- 3.1%
+	 val = 1-0.824;
+      }
+   } else {
+     // PbPb
+      val = 0.15; // place holder
+   }
+   return val;
 }
