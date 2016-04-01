a810b74f9c3518e127cb0505e5487638db8eb137
Jing Wang <boundino@gmail.com>
2016-04-01 23:50:04 +0200

    clear macro

--- CrossSection/plotPnNP.C
+++ CrossSection/plotPnNP.C
@@ -83,63 +83,6 @@ void plotPnNP(TString collision="PbPb",TString outputfileP="test.root",TString o
   legEff->Draw("same");
   cEff->SaveAs(Form("canvasEff_PnNP_%s.pdf",collision.Data()));
 
-
-  /*
-  TLatex* texY = new TLatex(0.52,0.81,"|y| < 1.0");
-  texY->SetNDC();
-  texY->SetTextFont(42);
-  texY->SetTextSize(0.04);
-  TLatex* texPt = new TLatex(0.52,0.76,Form("%.1f < p_{T} < %.1f GeV/c",ptMin,ptMax));
-  texPt->SetNDC();
-  texPt->SetTextFont(42);
-  texPt->SetTextSize(0.04);
-
-  TCanvas* cSideband = new TCanvas("cSideband","",600,600);
-  TLegend* legSideband = new TLegend(0.51,0.63,0.90,0.74);
-  legSideband->SetBorderSize(0);
-  legSideband->SetFillStyle(0);
-  legSideband->AddEntry(hSideband,"Sideband","pl");
-  legSideband->AddEntry(fSideband,"Fit","l");
-  hSideband->Draw();
-  fSideband->Draw("same");
-  texCms->Draw();
-  texCol->Draw();
-  texY->Draw();
-  texPt->Draw();
-  legSideband->Draw("same");
-  cSideband->SaveAs(Form("plots/Fits/cSideband_%s_%.0f_%.0f.pdf",collision.Data(),ptMin,ptMax));
-
-  TCanvas* cMCPSignal = new TCanvas("cMCPSignal","",600,600);
-  TLegend* legMCPSignal = new TLegend(0.51,0.63,0.88,0.74);
-  legMCPSignal->SetBorderSize(0);
-  legMCPSignal->SetFillStyle(0);
-  legMCPSignal->AddEntry(hMCPSignal,"Prompt","pl");
-  legMCPSignal->AddEntry(fMCPSignal,"Fit","l");
-  hMCPSignal->Draw();
-  fMCPSignal->Draw("same");
-  texCms->Draw();
-  texCol->Draw();
-  texY->Draw();
-  texPt->Draw();
-  legMCPSignal->Draw("same");
-  cMCPSignal->SaveAs(Form("plots/Fits/cMCPSignal_%s_%.0f_%.0f.pdf",collision.Data(),ptMin,ptMax));
-
-  TCanvas* cMCNPSignal = new TCanvas("cMCNPSignal","",600,600);
-  TLegend* legMCNPSignal = new TLegend(0.51,0.63,0.88,0.74);
-  legMCNPSignal->SetBorderSize(0);
-  legMCNPSignal->SetFillStyle(0);
-  legMCNPSignal->AddEntry(hMCNPSignal,"Non-prompt","pl");
-  legMCNPSignal->AddEntry(fMCNPSignal,"Fit","l");
-  hMCNPSignal->Draw();
-  fMCNPSignal->Draw("same");
-  texCms->Draw();
-  texCol->Draw();
-  texY->Draw();
-  texPt->Draw();
-  legMCNPSignal->Draw("same");
-  cMCNPSignal->SaveAs(Form("plots/Fits/cMCNPSignal_%s_%.0f_%.0f.pdf",collision.Data(),ptMin,ptMax));
-  */
-
 }
 
 int main(int argc, char* argv[])
