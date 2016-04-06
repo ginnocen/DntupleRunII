149972c94655756297710043f3005a93b4fda1b9
ginnocen <ginnocen@cern.ch>
2016-04-06 10:58:00 +0200

    new skim

--- CodeForNtupleProduction/skimForPbPbTrig.C
+++ CodeForNtupleProduction/skimForPbPbTrig.C
@@ -43,7 +43,7 @@ void AddCloneTree(vector<TTree*> &cloneForest,TFile *outf, TTree* t, const char 
 }
 
 // main routine
-void skimForPbPbTrig(TString infname="/data/dmeson2015/DataDntuple/nt_skimmed_DfinderData_PbPb_20160126_dPt0tkPt2p5_D0Dstar3p5p_FINALJSON_v6_partialstats_v2.root",TString outfname="/data/dmeson2015/DataDntuple/nt_skimmed_DfinderData_PbPb_20160126_dPt0tkPt2p5_D0Dstar3p5p_FINALJSON_v6_partialstats_v2_skimmed.root")
+void skimForPbPbTrig(TString infname="/data/wangj/Data2015/Dntuple/PbPb/ntD_EvtBase_20160331_DfinderData_PbPb_20160126_dPt0tkPt2p5_D0_FINALJSON_v6.root",TString outfname="/data/dmeson2015/DataDntuple/ntD_EvtBase_20160331_DfinderData_PbPb_20160126_dPt0tkPt2p5_D0_FINALJSON_v6_skimmed.root")
 {
    vector<TTree*> cloneForest;
    vector<TTree*> forest;
