cbf5d9759c30a226c47ebac73cda377807509a3c
ginnocen <ginnocen@cern.ch>
2016-04-18 15:35:41 +0200

    update pt shape

--- CrossSection/copyPlots.sh
+++ CrossSection/copyPlots.sh
@@ -19,6 +19,10 @@ cp "$MYPWD"/plotRAA/*.pdf Plots/plotRAA/.
 cp "$MYPWD"/plotEff/canvasTotAccEfficiencyPtHatWeight_isPbPb0.pdf Plots/Efficiency/.
 cp "$MYPWD"/plotEff/canvasTotAccEfficiencyPtHatWeight_isPbPb1.pdf Plots/Efficiency/.
 
+cp "$MYPWD"/plotEff/canvasPtreweightedComparisonPbPbCent010.pdf Plots/Efficiency/.
+cp "$MYPWD"/plotEff/canvasPtreweightedComparisonPbPbCent0100.pdf Plots/Efficiency/.
+cp "$MYPWD"/plotEff/canvasPtreweightedComparisonProtonProton.pdf Plots/Efficiency/.
+
 cp "$MYPWD"/canvasPtreweightedComparisonPP.pdf Plots/SystPtShape/.
 cp "$MYPWD"/canvasPtreweightedComparisonPPMB.pdf Plots/SystPtShape/.
 cp "$MYPWD"/canvasPtreweightedComparisonPbPb.pdf Plots/SystPtShape/.
