2b70214df4ec498307ece260a71a15a133a217d5
ginnocen <ginnocen@cern.ch>
2016-04-26 11:48:02 +0200

    final version of the code

---
+++ CrossSection/config/parametersLowptPbPb_extended.h
@@ -0,0 +1,13 @@
+
+const int nBins=4;
+double ptBins[nBins+1] = {15.,20.,25,30,40};
+const int nBinsReweight=50;
+double ptBinsReweight[nBinsReweight+1] = {0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100};
+
+//nothing from here on needs to be changed
+const int BIN_NUM= 1196;
+const int HMIN=1;
+const int HMAX=300;
+
+const double binsize=((double)(HMAX)-(double)(HMIN))/(double)(BIN_NUM);
+Double_t BRchain=0.0388;
