cacae3c142a6a28020019b9102b5a4b379bf8b91
ginnocen <ginnocen@cern.ch>
2016-04-30 20:30:27 +0200

    add

--- CrossSection/config/parametersLowpt_extended.h
+++ CrossSection/config/parametersLowpt_extended.h
@@ -1,6 +1,6 @@
 
-const int nBins=6;
-double ptBins[nBins+1] = {5.,10.,15.,20.,25,30,35};
+const int nBins=9;
+double ptBins[nBins+1] = {10.,15.,20.,25,30.,40.,50.,60.,80,100};
 const int nBinsReweight=50;
 double ptBinsReweight[nBinsReweight+1] = {0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100};
 
