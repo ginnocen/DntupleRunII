6f1b95d33e9feb3540fb6f8be9f8d201c5d4563b
gian michele innocenti <ginnocen@cern.ch>
2016-05-22 12:35:14 +0100

    adding trigger efficiency

--- CrossSection/plotForTalk/copyPlotsForTalk.sh
+++ CrossSection/plotForTalk/copyPlotsForTalk.sh
@@ -22,3 +22,6 @@ cp ../plotRAA/canvasRAAchargedParticle_0_100.pdf canvasRAAcharged0100.pdf
 cp ../plotRAA/canvasRAAchargedParticle_0_10.pdf canvasRAAcharged010.pdf
 cp ../plotRAA/canvasRAAComparisonAll_0_100.pdf canvasRAAtheory0100.pdf
 cp ../plotRAA/canvasRAAComparisonAll_0_10.pdf canvasRAAtheory010.pdf
+
+
+cp ../../TriggerStudies/trigPre/HLTDmesonEfficiency.pdf HLTDmesonEfficiency.pdf
